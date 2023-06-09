3957b99de42e7657c0d24587d0e600aed5ead5a6
PLTytus <34415908+PLTytus@users.noreply.github.com>
2023-06-09 11:27:46 +0200

    code

---
+++ index.js
@@ -0,0 +1,20 @@
+document.onreadystatechange = function(){
+	document.querySelectorAll("input[type=text]").forEach(input => {
+		input.addEventListener("input", function(){
+			this.value = this.value.trim().toUpperCase();
+
+			if(this.name == "float"){
+				this.value = this.value.replace(',', '.').replace(/[^0-9.]/g, '');
+				document.querySelector("[name=int]").value = new Int32Array(new Float32Array([this.value]).buffer)[0];
+			} else if(this.name == "int"){
+				this.value = this.value.replace(/[^0-9]/g, '');
+				document.querySelector("[name=float]").value = new Float32Array(new Int32Array([this.value]).buffer)[0];
+			}
+		});
+
+		input.addEventListener("click", function(){
+			this.focus();
+			this.select();
+		});
+	});
+}
