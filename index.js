0132547600cf642e73f5d43ed666ed7c901df89d
Rafał 'Tytus' Miętkiewski <pltytus@gtaweb.eu>
2023-06-21 10:41:19 +0200

    negative numbers

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ document.onreadystatechange = function(){
 			this.value = this.value.trim().toUpperCase();
 
 			if(this.name == "float"){
-				this.value = this.value.replace(',', '.').replace(/[^0-9.]/g, '');
+				this.value = this.value.replace(',', '.').replace(/[^0-9\.\-]/g, '');
 				document.querySelector("[name=int]").value = new Int32Array(new Float32Array([this.value]).buffer)[0];
 			} else if(this.name == "int"){
-				this.value = this.value.replace(/[^0-9]/g, '');
+				this.value = this.value.replace(/[^0-9\-]/g, '');
 				document.querySelector("[name=float]").value = new Float32Array(new Int32Array([this.value]).buffer)[0];
 			}
 		});
